1d91ce37b5c845114bab8e92aaa92a81949014e1
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-13 12:26:53 +0000

    Add dummy server, Add get request endpoints for users and user by id

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // implement your API here
+
+const express = require("express");
+
+const { find, findById, insert, update, remove } = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  findById(id)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      res.status(404).json(`user with the id of ${id} not found`);
+    });
+});
+
+app.listen(8000, () => {
+  console.log("listening on 8000");
+});
