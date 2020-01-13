e0026edaf04d6fde0f06e257b5131fd332835acc
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-13 15:35:18 +0000

    MVP Met and checked for results

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // implement your API here
 
 const express = require("express");
+const cors = require("cors");
 
 const { find, findById, insert, update, remove } = require("./data/db");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.post("/api/users", (req, res) => {
   const newUser = req.body;
@@ -78,28 +81,36 @@ app.delete("/api/users/:id", (req, res) => {
 app.put("/api/users/:id", (req, res) => {
   const { id } = req.params;
   const userToUpdate = req.body;
-  update(id, userToUpdate)
-    .then(user => {
-      if (user) {
-        const updatedUserInfo = findById(id);
-        res.status(200).json(updatedUserInfo);
-      } else {
-        res
-          .status(404)
-          .json({ message: "The user with the specified ID does not exist." });
-      }
-    })
-    .catch(error => {
-      if (!userToUpdate.name || !userToUpdate.bio) {
-        res
-          .status(400)
-          .json({ errorMessage: "Please provide name and bio for the user." });
-      } else {
+  if (!userToUpdate.name || !userToUpdate.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    update(id, userToUpdate)
+      .then(updated => {
+        if (!updated) {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        } else {
+          const updatedDetails = findById(id)
+            .then(user => {
+              res.status(200).json(user);
+            })
+            .catch(error => {
+              res.status(404).json({
+                message: "The user with the specified ID does not exist."
+              });
+              res.status(200).json(updatedDetails);
+            });
+        }
+      })
+      .catch(error => {
         res.status(500).json({
           errorMessage: "The user information could not be modified."
         });
-      }
-    });
+      });
+  }
 });
 
 app.listen(8000, () => {
