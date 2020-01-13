7479e123a64a31171e7fe40bcba368f0ccdd885c
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-13 12:58:20 +0000

    Add post endpoint

--- index.js
+++ index.js
@@ -14,18 +14,75 @@ app.get("/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(error => {
-      res.status(500).json({ message: error.message, stack: error.stack });
+      res
+        .status(500)
+        .json({ message: "The users information could not be retrieved." });
+    });
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  insert(newUser)
+    .then(user => {
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          errorMessage:
+            "There was an error while saving the user to the database"
+        });
     });
 });
 
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
   findById(id)
-    .then(data => {
-      console.log(data);
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({
+            errorMessage: "The user with the specified ID does not exist."
+          });
+      }
     })
     .catch(error => {
-      res.status(404).json(`user with the id of ${id} not found`);
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    });
+});
+
+// INSERT
+
+//update
+
+app.delete("./users/:id", (req, res) => {
+  const { id } = req.params;
+  remove(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(`The user with the id: ${id} has been removed`);
+      } else {
+        res
+          .status(404)
+          .json({
+            errorMessage: "The user with the specified ID does not exist."
+          });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
     });
 });
 
